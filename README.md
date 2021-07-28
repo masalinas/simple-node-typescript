79e60e6008b3f612074a95f88ca39a834b6b711e
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2021-07-28 19:25:21 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# simple-node-typescript
-A Simple Typescrip PoC from node and some rxjs operators
+# description
+A Simple typescript node project using rxjs library
+
+# steps
+step 01: create working folder
+mkdir simple-node-typescript
+
+cd simple-node-typescript
+
+step 02: initialize node project
+npm init
+
+we could remove main attribute in your package.json
+
+step 03: install dependencies
+npm install typescript --save
+npm install rxjs --save
+
+step04: create source a destination folders in your project
+mkdir src
+mkdir dist
+
+step05: add npm start script in your package.json to compile and start your project
+"start": "npx tsc && node dist/main.js",
---
+++ src/main.ts
@@ -0,0 +1,55 @@
+import { from, of } from 'rxjs';
+import { map, filter, reduce, groupBy, mergeMap, toArray  } from 'rxjs/operators';
+
+import { Product } from './product';
+
+let products: Product[] = [
+  {name: 'Apple', price: 12, active: true},
+  {name: 'Banana', price: 8, active: false},
+  {name: 'Orange', price: 10, active: true},
+];
+
+// list all products
+console.log('1) List all products');
+from(products)
+  .subscribe((product: Product) => {
+    console.log(product);
+  });
+console.log('\n');
+
+// filter active products
+console.log('2) Filter active products');
+from(products).pipe(filter(product => product.active === true))
+  .subscribe((product: Product) => {
+    console.log(product);
+  });
+console.log('\n');
+
+// map products colletion
+console.log('3) Create format product descriptions');
+from(products).pipe(map((product: Product) => product.name + 'with price ' + product.price))
+  .subscribe((message: string) => {
+    console.log(message);
+  });
+console.log('\n');
+
+console.log('4) Return array from filtered products');
+from(products).pipe(filter(product => product.active === true),
+                    reduce((results: Product[], product) => [...results, product], []))
+                      .subscribe((products) => {
+                        console.log(products);
+                      });
+console.log('Other way to Return array from filtered products');
+from(products).pipe(reduce((results: Product[], product) => product.active === true ? [...results, product] : results, []))
+  .subscribe((products) => {
+    console.log(products);
+  });
+console.log('\n');
+
+console.log('5) Group products by active status');
+from(products).pipe(groupBy(product => product.active),
+                    mergeMap(group => group.pipe(toArray())))
+    .subscribe(products => {
+      console.log(products);
+    });
+console.log('\n');
